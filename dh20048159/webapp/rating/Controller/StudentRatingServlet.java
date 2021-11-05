e4195a7cd0feac81615af3a4167bc432161d51ad
ravitejadharmishetty <93790027+ravitejadharmishetty@users.noreply.github.com>
2021-11-05 23:50:50 +0530

    Add files via upload

---
+++ dh20048159/webapp/rating/Controller/StudentRatingServlet.java
@@ -0,0 +1,10 @@
+package dh20048159.webapp.rating.Controller;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+public class StudentRatingServlet {
+
+}
